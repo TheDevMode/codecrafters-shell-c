399d644f236128e5b26b44dc83899cc19e3b2b94
TheDevMode <tmode277@gmail.com>
2026-08-15 13:42:27 +0000

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -4,12 +4,11 @@
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
-  while (1) 
-  {
     setbuf(stdout, NULL);
 
   // TODO: Uncomment the code below to pass the first stage
-  
+  while (1)
+  {
     printf("$ ");
     char input[100];
     fgets(input, 100, stdin);
