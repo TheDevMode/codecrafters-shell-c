ccee9bde4d24bb945f3ee9bb391613b48af366b3
TheDevMode <tmode277@gmail.com>
2026-08-17 22:19:11 +0000

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -12,21 +12,18 @@ int main(int argc, char *argv[]) {
     char input[100];
     fgets(input, 100, stdin);
 
+    // Remove the trailing newline
+    input[strlen(input) - 1] = '\0';
 
-   //Remove the trailing
-    input[strlen(input) - 1] = '\0' ;
-  
-  //strcompare
-   if (strcmp(input, "exit") == 0) {
+    // Compare input commands
+    if (strcmp(input, "exit") == 0) {
       break;
-  }
-  if (strncmp(input, "echo ", 5) == 0) {
+    } else if (strncmp(input, "echo ", 5) == 0) {
       printf("%s\n", input + 5);
-  }
-
+    } else {
       printf("%s: command not found\n", input);
-
-
+    }
   }
+
   return 0;
 }
