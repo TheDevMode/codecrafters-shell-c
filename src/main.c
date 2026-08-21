f0c45ce24e711d776f0a2573e0d3618497e6c888
TheDevMode <tmode277@gmail.com>
2026-08-21 17:18:15 +0000

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -38,6 +38,12 @@ int main(int argc, char *argv[]) {
     else if (strncmp(input, "cd ", 3) == 0) {
       if (chdir(input + 3) != 0) {
         printf("cd: %s: No such file or directory\n", input + 3);
+      if (strcmp(input + 3, "..") == 0) {
+        chdir("..");
+      if (strcmp(input + 3, ".") == 0) {
+        chdir(".");
+      if (strcmp(input + 3, "~") == 0) {
+        chdir(getenv("HOME"));
       }
     }
     else if (strncmp(input ,"type " , 5) == 0) {
