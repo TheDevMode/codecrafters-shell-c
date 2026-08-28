93060a1d8a4c222634ee487536f8c9666a45a925
TheDevMode <tmode277@gmail.com>
2026-08-28 12:07:51 +0000

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -2,31 +2,28 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <fcntl.h>
 
 int main(int argc, char *argv_main[]) {
-  // Flush after every printf
   setbuf(stdout, NULL);
-
-  char saved_path[512];
-  char *env_path = getenv("PATH");
 
   while (1) {
     printf("$ ");
-    char input[100];
+    fflush(stdout);
+
+    char input[512];
     if (fgets(input, sizeof(input), stdin) == NULL) {
       break;
     }
 
-    // Remove the trailing newline
     input[strcspn(input, "\n")] = '\0';
-    
-    char *argv[64];
+
+    char *argv[128];
     int argc_count = 0;
 
-    char buffer[256];
+    char buffer[512];
     int buffer_index = 0;
     
     char in_quotes = 0; 
@@ -36,7 +33,7 @@ int main(int argc, char *argv_main[]) {
       char c = input[i];
 
       if (is_escaped) {
-        if (buffer_index < sizeof(buffer) - 1) {
+        if (buffer_index < (int)sizeof(buffer) - 1) {
           buffer[buffer_index++] = c;
         }
         is_escaped = 0;
@@ -48,18 +45,18 @@ int main(int argc, char *argv_main[]) {
         else if (in_quotes == '"' && c == '\\') {
           char next = input[i + 1];
           if (next == '"' || next == '\\') {
-            if (buffer_index < sizeof(buffer) - 1) {
+            if (buffer_index < (int)sizeof(buffer) - 1) {
               buffer[buffer_index++] = next;
             }
             i++;
           } else {
-            if (buffer_index < sizeof(buffer) - 1) {
+            if (buffer_index < (int)sizeof(buffer) - 1) {
               buffer[buffer_index++] = c;
             }
           }
         } 
         else {
-          if (buffer_index < sizeof(buffer) - 1) {
+          if (buffer_index < (int)sizeof(buffer) - 1) {
             buffer[buffer_index++] = c;
           }
         }
@@ -72,20 +69,20 @@ int main(int argc, char *argv_main[]) {
         } else if (c == ' ') {
           if (buffer_index > 0) {
             buffer[buffer_index] = '\0';
-            if (argc_count < 63) {
+            if (argc_count < 127) {
               argv[argc_count++] = strdup(buffer);
             }
             buffer_index = 0;
           }
         } else {
-          if (buffer_index < sizeof(buffer) - 1) {
+          if (buffer_index < (int)sizeof(buffer) - 1) {
             buffer[buffer_index++] = c;
           }
         }
       }
     }
 
-    if (buffer_index > 0 && argc_count < 63) {
+    if (buffer_index > 0 && argc_count < 127) {
       buffer[buffer_index] = '\0';
       argv[argc_count++] = strdup(buffer);
     }
@@ -95,11 +92,8 @@ int main(int argc, char *argv_main[]) {
       continue;
     }
 
-    int total_allocated = argc_count; // Keep track of all allocated strings for freeing
+    int total_allocated = argc_count;
 
-    // =========================================================
-    // 2. CHECK FOR OUTPUT REDIRECTION (> OR 1>)
-    // =========================================================
     char *outfile = NULL;
     int redirect_idx = -1;
 
@@ -113,7 +107,6 @@ int main(int argc, char *argv_main[]) {
       }
     }
 
-    // Truncate argv for command execution
     if (redirect_idx != -1) {
       argv[redirect_idx] = NULL;
       argc_count = redirect_idx;
@@ -129,84 +122,94 @@ int main(int argc, char *argv_main[]) {
       }
     }
 
-    //exit
-    if (strcmp(argv[0], "exit") == 0) {
-      for (int i = 0; i < argc_count; i++) free(argv[i]);
-      break;
-    }
-    // echo
-    else if (strcmp(argv[0], "echo") == 0) {
-      for (int i = 1; i < argc_count; i++) {
-        printf("%s%s", argv[i], (i == argc_count - 1) ? "" : " ");
+    if (argc_count > 0) {
+      // exit
+      if (strcmp(argv[0], "exit") == 0) {
+        for (int i = 0; i < total_allocated; i++) free(argv[i]);
+        dup2(saved_stdout, STDOUT_FILENO);
+        close(saved_stdout);
+        break;
       }
-      printf("\n");
-    }
-    // pwd
-    else if (strcmp(argv[0], "pwd") == 0) {
-      char cwd[512];
-      if (getcwd(cwd, sizeof(cwd)) != NULL) {
-        printf("%s\n", cwd);
-      } else {
-        printf("getcwd error\n");
-      }
-    }
-    // cd
-    else if (strcmp(argv[0], "cd") == 0) {
-      const char *directory = argv[1];
-      if (directory != NULL && strcmp(directory, "~") == 0) {
-        directory = getenv("HOME");
-      }
-      if (directory == NULL || chdir(directory) != 0) {
-        printf("cd: %s: No such file or directory\n", argv[1] ? argv[1] : "");
-      }
-    }
-    // type
-    else if (strcmp(argv[0], "type") == 0) {
-      if (argv[1] == NULL) {
-        for (int i = 0; i < argc_count ; i++) free(argv[i]);
-        continue;
-      }
-      if (strcmp(argv[1], "exit") == 0 || strcmp(argv[1], "echo") == 0 ||
-          strcmp(argv[1], "pwd") == 0  || strcmp(argv[1], "cd") == 0 ||
-          strcmp(argv[1], "type") == 0) {
-        printf("%s is a shell builtin\n", argv[1]);
-      } else {
-        int found = 0;
-        if (env_path != NULL) {
-          strncpy(saved_path, env_path, sizeof(saved_path) - 1);
-          saved_path[sizeof(saved_path) - 1] = '\0';
+      // echo
+      else if (strcmp(argv[0], "echo") == 0) {
+        for (int i = 1; i < argc_count; i++) {
+          printf("%s%s", argv[i], (i == argc_count - 1) ? "" : " ");
         }
-        for (char *path = strtok(saved_path, ":"); path != NULL; path = strtok(NULL, ":")) {
-          char full_path[512];
-          snprintf(full_path, sizeof(full_path), "%s/%s", path, argv[1]);
-          if (access(full_path, X_OK) == 0) {
-            printf("%s is %s\n", argv[1], full_path);
-            found = 1;
-            break;
+        printf("\n");
+      }
+      // pwd
+      else if (strcmp(argv[0], "pwd") == 0) {
+        char cwd[512];
+        if (getcwd(cwd, sizeof(cwd)) != NULL) {
+          printf("%s\n", cwd);
+        } else {
+          printf("getcwd error\n");
+        }
+      }
+      // cd
+      else if (strcmp(argv[0], "cd") == 0) {
+        const char *directory = argv[1];
+        if (directory != NULL && strcmp(directory, "~") == 0) {
+          directory = getenv("HOME");
+        }
+        if (directory == NULL || chdir(directory) != 0) {
+          printf("cd: %s: No such file or directory\n", argv[1] ? argv[1] : "");
+        }
+      }
+      // type
+      else if (strcmp(argv[0], "type") == 0) {
+        if (argv[1] != NULL) {
+          if (strcmp(argv[1], "exit") == 0 || strcmp(argv[1], "echo") == 0 ||
+              strcmp(argv[1], "pwd") == 0  || strcmp(argv[1], "cd") == 0 ||
+              strcmp(argv[1], "type") == 0) {
+            printf("%s is a shell builtin\n", argv[1]);
+          } else {
+            int found = 0;
+            char *env_path = getenv("PATH");
+            if (env_path != NULL) {
+              char path_copy[1024];
+              strncpy(path_copy, env_path, sizeof(path_copy) - 1);
+              path_copy[sizeof(path_copy) - 1] = '\0';
+
+              for (char *path = strtok(path_copy, ":"); path != NULL; path = strtok(NULL, ":")) {
+                char full_path[1024];
+                snprintf(full_path, sizeof(full_path), "%s/%s", path, argv[1]);
+                if (access(full_path, X_OK) == 0) {
+                  printf("%s is %s\n", argv[1], full_path);
+                  found = 1;
+                  break;
+                }
+              }
+            }
+            if (!found) {
+              printf("%s not found\n", argv[1]);
+            }
           }
         }
-        if (!found) {
-          printf("%s not found\n", argv[1]);
-        }
       }
-    }
-   
-    else {
-      pid_t pid = fork();
-      if (pid == 0) {
-        execvp(argv[0], argv);
-        printf("%s: command not found\n", argv[0]);
-        exit(EXIT_FAILURE);
-      } else if (pid > 0) {
-        wait(NULL);
-      } else {
-        perror("fork");
-        exit(EXIT_FAILURE);
+      // External commands
+      else {
+        pid_t pid = fork();
+        if (pid == 0) {
+          execvp(argv[0], argv);
+          printf("%s: command not found\n", argv[0]);
+          exit(EXIT_FAILURE);
+        } else if (pid > 0) {
+          wait(NULL);
+        } else {
+          perror("fork");
+          exit(EXIT_FAILURE);
+        }
       }
     }
 
-    // Clean up allocated memory 
-    for (int i = 0; i < argc_count ; i++) {
+    // Flush and restore standard stdout
+    fflush(stdout);
+    dup2(saved_stdout, STDOUT_FILENO);
+    close(saved_stdout);
+
+    // Free memory
+    for (int i = 0; i < total_allocated; i++) {
       free(argv[i]);
     }
   }
